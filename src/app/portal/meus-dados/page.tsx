c5f06579648df63ede7f10068b446197a4cc59c1
gabrielyin <2021gabrielyin@gmail.com>
2023-06-16 00:03:13 -0300

    [FEATURE] criar tela de meus dados

---
+++ src/app/portal/meus-dados/page.tsx
@@ -0,0 +1,33 @@
+import Button from '@/src/components/Button'
+import PortalNav from '@/src/components/PortalNav'
+import TextInput from '@/src/components/TextInput'
+
+export default function MeusDados() {
+  return (
+    <div className="mx-auto grid max-w-6xl gap-8 lg:grid-cols-[300px,auto]">
+      <PortalNav />
+
+      <form className="grid gap-5 lg:max-w-lg">
+        <section>
+          <h3 className="font-sec text-2xl font-semibold">Perfil</h3>
+          <span>
+            <p className="leading-tight">
+              Essas são as informações usadas para acessar a sua conta no portal
+              do Maquinare
+            </p>
+          </span>
+        </section>
+
+        <section className="grid gap-3">
+          <TextInput label="E-mail" placeholder="email@gmail.com" />
+
+          <TextInput label="Nome" placeholder="Gabriel Yin" />
+
+          <TextInput label="Telefone" placeholder="(21) 98372-8281" />
+        </section>
+
+        <Button text="Atualizar" className="w-36" />
+      </form>
+    </div>
+  )
+}
