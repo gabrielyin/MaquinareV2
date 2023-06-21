990ba2fe1e16b30a052a01aa133d3962de93d9e0
gabrielyin <2021gabrielyin@gmail.com>
2023-06-21 20:08:04 -0300

    [FEATURE][MAQ-26] criar rota UPDATE para user

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -49,3 +49,20 @@ export async function GET(request: Request) {
 
   return NextResponse.json(user)
 }
+
+export async function PATCH(request: Request) {
+  const { searchParams } = new URL(request.url)
+  const id = searchParams.get('id')
+  const body = await request.json()
+
+  if (!id) {
+    return NextResponse.json({ message: 'ID not found' }, { status: 404 })
+  }
+
+  const updatedUser = await prisma.user.update({
+    where: { id },
+    data: body,
+  })
+
+  return NextResponse.json(updatedUser)
+}
