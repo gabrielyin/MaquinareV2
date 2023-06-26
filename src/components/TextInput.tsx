4666bc23a213336d8849f8d81cce1f48fe419092
gabrielyin <2021gabrielyin@gmail.com>
2023-06-26 16:37:23 -0300

    [HOTFIX][MAQ-41] Alterar textinput para tsx

--- src/components/TextInput.tsx
+++ src/components/TextInput.tsx
@@ -1,4 +1,15 @@
-export default function TextInput({ label, register, ...rest }) {
+import { InputHTMLAttributes } from 'react'
+
+interface TextInputInteface extends InputHTMLAttributes<HTMLInputElement> {
+  label: string
+  register?: any
+}
+
+export default function TextInput({
+  label,
+  register,
+  ...rest
+}: TextInputInteface) {
   return (
     <label className="block">
       <span className="mb-1 block font-sec">{label}</span>
