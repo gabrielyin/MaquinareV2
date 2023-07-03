facb0874770f1d7a32f34da8dbc3e8c7675052d9
gabrielyin <2021gabrielyin@gmail.com>
2023-07-03 15:37:30 -0300

    [CHORE] hotfix do baseurl axios

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -3,7 +3,7 @@ import axios from 'axios'
 const baseURL =
   process.env.NODE_ENV === 'development'
     ? 'http://localhost:3000'
-    : 'https://api.example.com'
+    : 'https://maquinare.vercel.app'
 
 export const api = axios.create({
   baseURL,
