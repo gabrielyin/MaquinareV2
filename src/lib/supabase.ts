c64682571aff0b4de9f647d079b520cd222a13d4
gabrielyin <2021gabrielyin@gmail.com>
2023-07-06 00:07:28 -0300

    [CHORE] renomear env variables to load on client side

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://qighkjkmhfikasepsjku.supabase.co'
-const supabaseKey =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFpZ2hramttaGZpa2FzZXBzamt1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2Njk4NTc5OTMsImV4cCI6MTk4NTQzMzk5M30.d8vRAe4aqZghqxiCSiXsfcA45oxQW3XLh49BdoA2p5I'
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY
 
-export const supabase = createClient(supabaseUrl, supabaseKey)
+export const supabase = createClient(supabaseUrl!, supabaseKey!)
